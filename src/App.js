6d8af714ff380e0662ea74ecf8f8a8865318135c
A.J. May <aj7may@gmail.com>
2018-07-28 10:38:48 -0400

    addressed comments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -7,26 +7,21 @@ import Grid from '@material-ui/core/Grid';
 import './App.css';
 import pranks from './pranks.json';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <AppBar>
-          <Toolbar>
-            <Typography variant="title" color="inherit">
-              &gt; Say Butt
-            </Typography>
-          </Toolbar>
-        </AppBar>
+const App = () => (
+  <Fragment>
+    <AppBar>
+      <Toolbar>
+        <Typography variant="title" color="inherit">
+          &gt; Say Butt
+        </Typography>
+      </Toolbar>
+    </AppBar>
 
-        <Grid container spacing={24}>
-          {pranks.map((prank, i) => (<Grid item xs={12} md={6} xl={4} key={i}>
-            <Prank name={prank.name} description={prank.description} />
-          </Grid>))}
-        </Grid>
-      </div>
-    );
-  }
-}
+    <Grid container spacing={24}>
+      {pranks.map((prank, i) => (<Grid item xs={12} md={6} xl={4} key={i}>
+        <Prank name={prank.name} description={prank.description} />
+      </Grid>))}
+    </Grid>
+  </Fragment>);
 
 export default App;
