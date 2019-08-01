aa83ca6d21ce18809566d2d175d0e1c4d9d234af
A.J. May <aj7may@gmail.com>
2019-08-01 11:30:31 -0400

    feat: add google analytics code

--- build.js
+++ build.js
@@ -110,6 +110,16 @@ pre, button {
 
 const index = `<html>
   <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-125158442-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-125158442-1');
+    </script>
+
     <title>&gt; saybutt</title>
     <style>
       ${styles}
